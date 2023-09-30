90b655d7d66f067f34338b132065b7f9e9d0f69f
Lucas Blomhäll <lucas.blomhall@gmail.com>
2023-10-01 01:25:09 +0200

    new commit

---
+++ FamilyFastFoodSimpleApi/DataModels/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FamilyFastFoodSimpleApi.DataModels
+{
+    public class UserDto
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+    }
+}
--- FamilyFastFoodSimpleApi/Program.cs
+++ FamilyFastFoodSimpleApi/Program.cs
@@ -9,6 +9,8 @@ builder.Services.AddDbContext<FamilyFastFoodSimpleApiContext>(options =>
 
 // Add services to the container.
 
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
